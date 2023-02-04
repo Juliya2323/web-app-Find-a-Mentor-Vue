b9bdde60d0d8893b6c183c2e01fcf3d1af6fba96
Juliya2323 <106690898+Juliya2323@users.noreply.github.com>
2023-02-04 20:26:39 +0300

    add auth component

--- src/pages/coaches/CoachList.vue
+++ src/pages/coaches/CoachList.vue
@@ -1,32 +1,36 @@
 <template>
   <div>
     <base-dialog :show="!!error" title="An error occured!" @close="handleError">
-    <p>{{ error }}</p>
-  </base-dialog>
-  <section class="coaches">
-    <coach-filter @change-filter="setFilters"></coach-filter>
-    <base-card>
-      <div class="coaches_controls">
-        <base-button mode="outline" @click="loadCoaches(true)">Refresh</base-button>
-        <base-button v-if="!isCoach && !isLoading" link to="/register">Register as a Coach</base-button>
-      </div>
-      <div v-if="isLoading">
-        <base-spinner></base-spinner>
-      </div>
-      <ul class="coaches_list" v-else-if="hasCoaches">
-        <coach-item
-          v-for="coach in filteredCoaches"
-          :key="coach.id"
-          :id="coach.id"
-          :firstName="coach.firstName"
-          :lastName="coach.lastName"
-          :rate="coach.hourlyRate"
-          :areas="coach.areas"
-        ></coach-item>
-      </ul>
-      <h3 v-else>No coaches found.</h3>
-    </base-card>
-  </section>
+      <p>{{ error }}</p>
+    </base-dialog>
+    <section class="coaches">
+      <coach-filter @change-filter="setFilters"></coach-filter>
+      <base-card>
+        <div class="coaches_controls">
+          <base-button mode="outline" @click="loadCoaches(true)"
+            >Refresh</base-button
+          >
+          <base-button v-if="!isCoach && !isLoading" link to="/register"
+            >Register as a Coach</base-button
+          >
+        </div>
+        <div v-if="isLoading">
+          <base-spinner></base-spinner>
+        </div>
+        <ul class="coaches_list" v-else-if="hasCoaches">
+          <coach-item
+            v-for="coach in filteredCoaches"
+            :key="coach.id"
+            :id="coach.id"
+            :firstName="coach.firstName"
+            :lastName="coach.lastName"
+            :rate="coach.hourlyRate"
+            :areas="coach.areas"
+          ></coach-item>
+        </ul>
+        <h3 v-else>No coaches found.</h3>
+      </base-card>
+    </section>
   </div>
 </template>
 
@@ -37,30 +41,30 @@ import CoachFilter from '../../components/coaches/CoachFilter.vue';
 export default {
   data() {
     return {
-        activeFilters: {
-            frontend: true,
-            backend: true,
-            career: true
-        },
-        isLoading: false,
-        error: null
-    }
+      activeFilters: {
+        frontend: true,
+        backend: true,
+        career: true,
+      },
+      isLoading: false,
+      error: null,
+    };
   },
   computed: {
     filteredCoaches() {
       const coaches = this.$store.getters['coaches/coaches'];
-      return coaches.filter(coach => {
+      return coaches.filter((coach) => {
         if (this.activeFilters.frontend && coach.areas.includes('frontend')) {
-            return true;
-        } 
+          return true;
+        }
         if (this.activeFilters.backend && coach.areas.includes('backend')) {
-            return true;
-        } 
+          return true;
+        }
         if (this.activeFilters.career && coach.areas.includes('career')) {
-            return true;
-        } 
+          return true;
+        }
         return false;
-      })
+      });
     },
     hasCoaches() {
       return !this.isLoading && this.$store.getters['coaches/hasCoaches'];
@@ -68,7 +72,6 @@ export default {
     isCoach() {
       return this.$store.getters['coaches/isCoach'];
       // если пользователь уже зарегистрирован как coach то убираем кнопку для регистрации через v-if
-        
     },
   },
   created() {
@@ -77,13 +80,15 @@ export default {
   components: { CoachItem, CoachFilter },
   methods: {
     setFilters(updatedFilters) {
-        this.activeFilters = updatedFilters;
+      this.activeFilters = updatedFilters;
     },
     async loadCoaches(refresh = false) {
       this.isLoading = true;
       try {
-        await this.$store.dispatch('coaches/loadCoaches', {forceRefresh: refresh});
-      } catch(error) {
+        await this.$store.dispatch('coaches/loadCoaches', {
+          forceRefresh: refresh,
+        });
+      } catch (error) {
         this.error = error.message || 'Something went wrong!';
       }
       //дожидаемся завершения через await и ставим статус загрузки на false
@@ -91,8 +96,8 @@ export default {
     },
     handleError() {
       this.error = null;
-    }
-  }
+    },
+  },
 };
 </script>
 
